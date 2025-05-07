41d5fd1c98ad564d0b3520a8187ac40eac7c08e2
quan0715 <quan787887@gmail.com>
2025-05-07 22:11:27 +0800

    add UserWeaklyReviewCard  and InprogressTaskCard and AssignedTaskCard

---
+++ src/views/Task/AssignedTaskCard.vue
@@ -0,0 +1,62 @@
+<template>
+  <DashboardCard title="已指派（代完成）">
+    <div class="flex flex-col gap-3">
+      <div
+        v-for="task in getTasks"
+        :key="task.id"
+        class="flex flex-col justify-start items-start gap-2 rounded-md p-3 border-2 border-gray-100"
+      >
+        <div class="px-2 bg-purple-50 rounded-xl">
+          <span class="text-[12px] text-sans font-semibold text-purple-600">{{ task.code }}</span>
+        </div>
+        <p class="text-md text-sans font-semibold text-gray-600">
+          {{ task.name }}
+        </p>
+        <div class="flex flex-row justify-start items-start gap-2">
+          <p class="text-sm text-sans font-thin text-gray-500">
+            {{ task.requirement.join(' ') }}
+          </p>
+        </div>
+      </div>
+    </div>
+  </DashboardCard>
+</template>
+
+<script setup lang="ts">
+import DashboardCard from '@/components/DashboardCard.vue'
+import { computed } from 'vue'
+const getTasks = computed(() => {
+  return [
+    {
+      id: 1,
+      code: '溫度測試',
+      name: 'XD12345',
+      requirement: ['#專業1', '#專業2', '#專業3'],
+    },
+    {
+      id: 2,
+      code: '溫度測試',
+      name: 'XD12346',
+      requirement: ['#專業1', '#專業2', '#專業3'],
+    },
+    {
+      id: 3,
+      code: '溫度測試',
+      name: 'XD12347',
+      requirement: ['#專業1', '#專業2', '#專業3'],
+    },
+    {
+      id: 4,
+      code: '溫度測試',
+      name: 'XD12348',
+      requirement: ['#專業1', '#專業2', '#專業3'],
+    },
+    {
+      id: 5,
+      code: '溫度測試',
+      name: 'XD12349',
+      requirement: ['#專業1', '#專業2', '#專業3'],
+    },
+  ]
+})
+</script>
---
+++ src/views/Task/InprogressTaskCard.vue
@@ -0,0 +1,92 @@
+<template>
+  <DashboardCard title="進行中任務">
+    <div v-if="inprogressTask">
+      <div class="flex flex-col justify-start items-start gap-2 p-2">
+        <div class="px-2 py-1 bg-purple-50 rounded-xl">
+          <span class="text-sm text-sans font-semibold text-purple-600">{{
+            inprogressTask.taskCode
+          }}</span>
+        </div>
+        <p
+          class="text-2xl text-sans font-semibold text-gray-600"
+          ass="text-sm text-sans font-semibold text-gray-600"
+        >
+          {{ inprogressTask.taskName }}
+        </p>
+      </div>
+      <Separator class="w-full my-2" />
+      <div class="grid grid-cols-3 gap-2">
+        <div
+          v-for="(machine, idx) in inprogressTask.machine"
+          :key="machine"
+          class="col-span-1 flex flex-row justify-start items-start gap-2"
+        >
+          <DashboardData title="佔用機器" :content="machine" />
+          <Separator orientation="vertical" v-if="idx !== inprogressTask.machine.length - 1" />
+        </div>
+      </div>
+      <Separator class="w-full my-2" />
+      <DashboardData title="開始時間" :content="formatDuration(elapsedSeconds)" />
+      <Separator class="w-full my-2" />
+      <div class="w-full flex flex-row justify-center items-center gap-2 p-2">
+        <Button variant="secondary" class="w-full">
+          <span class="text-sm text-sans font-semibold">取消任務</span>
+        </Button>
+        <Button variant="default" class="w-full">
+          <span class="text-sm text-sans font-semibold">完成任務</span>
+        </Button>
+      </div>
+    </div>
+    <div v-else>
+      <p class="text-sm text-sans font-thin text-gray-600">沒有進行中的任務</p>
+    </div>
+  </DashboardCard>
+</template>
+
+<script setup lang="ts">
+import DashboardCard from '@/components/DashboardCard.vue'
+import DashboardData from '@/components/DashboardData.vue'
+import { ref, onMounted, onUnmounted, computed } from 'vue'
+import type { InprogressTask } from '@/types/task'
+import { Separator } from '@/components/ui/separator'
+import { Button } from '@/components/ui/button'
+// mock fetching data from repo
+const getInprogressTask = (): InprogressTask | undefined => {
+  return {
+    id: 1,
+    taskCode: '溫度測試',
+    taskName: 'XD12334131',
+    user: 'QUAN',
+    machine: ['Machine1', 'Machine2', 'Machine3'],
+    startTime: new Date('2025-05-07 20:00:00'),
+  }
+}
+
+const inprogressTask = ref<InprogressTask | undefined>(getInprogressTask())
+
+const startTime = ref(inprogressTask.value?.startTime ?? new Date())
+const now = ref(new Date())
+
+let timer: number | undefined
+
+onMounted(() => {
+  timer = setInterval(() => {
+    now.value = new Date()
+  }, 1000)
+})
+
+onUnmounted(() => {
+  clearInterval(timer)
+})
+
+const elapsedSeconds = computed(() => {
+  return Math.floor((now.value.getTime() - startTime.value.getTime()) / 1000)
+})
+
+function formatDuration(sec: number) {
+  const h = Math.floor(sec / 3600)
+  const m = Math.floor((sec % 3600) / 60)
+  const s = sec % 60
+  return `${h} 小時 ${m.toString().padStart(2, '0')} 分鐘 ${s.toString().padStart(2, '0')} 秒`
+}
+</script>

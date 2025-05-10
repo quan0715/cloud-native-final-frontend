506cdf47626323bbb0e608af7d492868a7b92f18
quan0715 <quan787887@gmail.com>
2025-05-11 01:52:11 +0800

    update repo and composable function

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -5,33 +5,17 @@
       <span class="text-blue-600">{{ $route.meta.title }}</span>
     </h1>
     <div class="flex items-center gap-4 h-[48px]">
-      <UserHeader
-        :username="username"
-        lab-name="lab11"
-        :role-label="roleLabel"
-        @logout="handleLogout"
-      />
+      <UserHeader :username="username" lab-name="lab11" :role-label="roleLabel" @logout="logout" />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import UserHeader from '@/components/User/UserHeader.vue'
-import { useRouter } from 'vue-router'
-import { jwtDecode } from 'jwt-decode'
-const token = localStorage.getItem('token') || ''
-const decodedToken = jwtDecode(token) as { id: string; role: string }
-const username = decodedToken.id
-const roleLabel = decodedToken.role
+import { useLogout } from '@/composables/useLogout'
+import { useUserData } from '@/composables/useUserData'
+const { logout } = useLogout()
+const { username, roleLabel } = useUserData()
 
 const envTag = import.meta.env.VITE_ENV_TAG
-
-// const emit = defineEmits(['logout'])
-const router = useRouter()
-
-function handleLogout() {
-  localStorage.removeItem('token')
-  router.push('/login')
-  // emit('logout')
-}
 </script>
